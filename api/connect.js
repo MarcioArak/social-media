8c833813e92a52868d95736eae830393a12ea273
MarcioArak <marcio.arakaki22@gmail.com>
2024-05-28 01:29:43 -0700

    conneting to railway DB

--- api/connect.js
+++ api/connect.js
@@ -1,9 +1,15 @@
 import mysql from "mysql";
 import "dotenv/config";
 
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+// Connect through railway
+const urlDB = `mysql://root:IldzwUSHqwXCkQzpYMyVMqxQiPxpSmeL@roundhouse.proxy.rlwy.net:37515/railway`;
+export const db = mysql.createConnection(urlDB);
+
+// Connect locally
+
+// export const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
