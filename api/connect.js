426e0558d2659c99d68ff380c7b9ba2cc5fcc33a
Marcio Arakaki <43198562+MarcioArak@users.noreply.github.com>
2024-05-29 14:26:13 -0700

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -2,14 +2,13 @@ import mysql from "mysql";
 import "dotenv/config";
 
 // Connect through railway
-const urlDB = `mysql://root:IldzwUSHqwXCkQzpYMyVMqxQiPxpSmeL@roundhouse.proxy.rlwy.net:37515/railway`;
-export const db = mysql.createConnection(urlDB);
+// const urlDB = `mysql://root:IldzwUSHqwXCkQzpYMyVMqxQiPxpSmeL@roundhouse.proxy.rlwy.net:37515/railway`;
+// export const db = mysql.createConnection(urlDB);
 
 // Connect locally
-
-// export const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-// });
+export const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
